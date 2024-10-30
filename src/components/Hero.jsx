0ed25290c4e1d0f29693fca3ab1bd6908ff90840
Rajat Patel <patelrajat394120@gmail.com>
2024-10-30 15:03:46 +0530

    Commit no.4

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,69 +1,93 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 import { motion } from "framer-motion";
+import Typewriter from 'typewriter-effect';
 
-export default function Experience() {
-  const [experiences, setExperiences] = useState([]);
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
+
+export default function Hero() {
+  const [heroData, setHeroData] = useState(null);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    async function fetchExperiences() {
+    const fetchHeroData = async () => {
       try {
-        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/experiences`);
-        if (!response.ok) throw new Error("Failed to fetch experiences data");
+        const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/hero`);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
         const data = await response.json();
-        setExperiences(data);
+        setHeroData(data.hero);
       } catch (error) {
-        console.error("Error fetching experiences:", error);
+        setError(error.message);
       }
-    }
-    fetchExperiences();
+    };
+
+    fetchHeroData();
   }, []);
 
-  if (experiences.length === 0) {
+  if (error) {
+    return <div>Error fetching hero data: {error}</div>;
+  }
+
+  if (!heroData) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="border-b border-neutral-800 pb-4">
-      <motion.h1
-        whileInView={{ opacity: 1, y: 0 }}
-        initial={{ opacity: 0, y: -100 }}
-        transition={{ duration: 0.5 }}
-        className="my-20 text-center text-4xl"
-      >
-        Experience
-      </motion.h1>
-      <div>
-        {experiences.map((experience, index) => (
-          <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: -100 }}
-              transition={{ duration: 1 }}
-              className="w-full lg:w-1/4"
-            >
-              <p className="mb-2 text-sm text-neutral-400">{experience.year}</p>
-            </motion.div>
-            <motion.div
-              whileInView={{ opacity: 1, x: 0 }}
-              initial={{ opacity: 0, x: 100 }}
-              transition={{ duration: 1 }}
-              className="w-full max-w-xl lg:w-3/4"
-            >
-              <h6 className="mb-2 font-semibold">
-                {experience.role} - <span className="text-sm text-purple-100">{experience.company}</span>
-              </h6>
-              <p className="mb-4 text-neutral-400">{experience.description}</p>
-              {experience.technologies.map((tech, techIndex) => (
-                <span
-                  key={techIndex}
-                  className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800"
-                >
-                  {tech}
-                </span>
-              ))}
-            </motion.div>
+    <div className="border-b border-neutral-900 pb-4 lg:mb-35">
+      <div className="flex flex-wrap">
+        <div className="w-full lg:w-1/2">
+          <div className="flex flex-col items-center lg:items-start">
+            <motion.h1 
+              variants={container(0)}
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+              Rajat Patel
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)}
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-blue-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+              <Typewriter
+                options={{
+                  strings: ['Full Stack Developer!', 'Data Analyst!'],
+                  autoStart: true,
+                  loop: true,
+                  deleteSpeed: 20,
+                  cursor: "|",
+                }}
+              />
+            </motion.span>
+            <motion.p
+              variants={container(1)}
+              initial="hidden"
+              animate="visible"
+              className="my-2 max-w-xl py-6 font-light tracking-tighter">
+              {heroData.heroContent}
+            </motion.p>
           </div>
-        ))}
+        </div>
+        <div className="w-full lg:w-1/2 lg:p-8">
+          <div className="flex justify-center">
+            <motion.img
+              className="rounded-full"
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              src={heroData.imageUrl}
+              alt="Rajat Patel"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
